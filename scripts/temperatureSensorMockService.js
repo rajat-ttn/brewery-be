ce4dd118c3b5eadd291a8e05157d5f2458649bb8
Pooja-22 <pooja.garg@tothenew.com>
2018-03-08 16:02:36 +0530

    Added queue producer

--- scripts/temperatureSensorMockService.js
+++ scripts/temperatureSensorMockService.js
@@ -1,36 +1,49 @@
 const rp = require('request-promise');
 const cron = require('node-cron');
 const _ = require('lodash');
+const amqp = require('amqplib/callback_api');
 
-const postOptions = {
-    method: 'POST',
-    body: {
-        updatedTemperature:null
-    },
-    json: true // Automatically stringifies the body to JSON
-};
-
-const beers = require('../mockData/beers/beers.json').beers;
-
-const containers = _.chain(beers).map('containerId').value();
-
-cron.schedule('*/10 * * * * *', function(){
-    containers.forEach((containerId) => {
-        notifyContainerTemperature(containerId);
+amqp.connect('amqp://localhost', (err, conn) => {
+    console.log(err, conn, 'testing step1');
+    conn.createChannel((err, ch) => {
+        console.log(err, ch, 'testing step2');
+        const queueName = 'TEMPERATURE_QUEUE';
+        ch.assertQueue(queueName, {durable: false});
+        ch.sendToQueue(queueName, new Buffer('Testing sending message!'));
+        console.log("testing step3")
     });
-    console.log('cron task just got executed!');
 });
 
-function notifyContainerTemperature(id){
-    const randomTemp = getRandomArbitrary(3,7);
-    postOptions.uri =`http://localhost:3001/api/containers/${id}/updateTemperature`;
-    postOptions.body.updatedTemperature = randomTemp;
-    rp(postOptions);
-}
 
-/**
- * Returns a random number between min (inclusive) and max (exclusive)
- */
-function getRandomArbitrary(min, max) {
-    return Math.floor(Math.random() * (max - min + 1)) + min;
-}
+// const postOptions = {
+//     method: 'POST',
+//     body: {
+//         updatedTemperature:null
+//     },
+//     json: true // Automatically stringifies the body to JSON
+// };
+//
+// const beers = require('../mockData/beers/beers.json').beers;
+//
+// const containers = _.chain(beers).map('containerId').value();
+
+// cron.schedule('*/10 * * * * *', function(){
+//     containers.forEach((containerId) => {
+//         notifyContainerTemperature(containerId);
+//     });
+//     console.log('cron task just got executed!');
+// });
+//
+// function notifyContainerTemperature(id){
+//     const randomTemp = getRandomArbitrary(3,7);
+//     postOptions.uri =`http://localhost:3001/api/containers/${id}/updateTemperature`;
+//     postOptions.body.updatedTemperature = randomTemp;
+//     rp(postOptions);
+// }
+//
+// /**
+//  * Returns a random number between min (inclusive) and max (exclusive)
+//  */
+// function getRandomArbitrary(min, max) {
+//     return Math.floor(Math.random() * (max - min + 1)) + min;
+// }
